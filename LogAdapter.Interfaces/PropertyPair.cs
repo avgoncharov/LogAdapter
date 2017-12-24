86bd88d87846b206893497b192e7d8fe23a54890
avgoncharov <goncharov.andrey.v@gmail.com>
2017-12-24 23:19:22 +0300

    Start.

---
+++ LogAdapter.Interfaces/PropertyPair.cs
@@ -0,0 +1,29 @@
+﻿namespace LogAdapter.Interfaces
+{
+	/// <summary>
+	/// Class represents a pair of name-value. A log event will be enriched by this pair.
+	/// </summary>
+	public sealed class PropertyPair
+	{
+		/// <summary>
+		/// Create new instance of class.
+		/// </summary>
+		/// <param name="name">Name of property.</param>
+		/// <param name="value">Value of property.</param>
+		public PropertyPair(string name, object value)
+		{
+			Name = name;
+			Value = value;
+		}
+
+		/// <summary>
+		/// Get name of property.
+		/// </summary>
+		public string Name{ get; } 
+
+		/// <summary>
+		/// Get value of property.
+		/// </summary>
+		public object Value { get; }
+	}
+}
---
+++ LogAdapter.SerilogAdapter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogAdapter.SerilogAdapter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogAdapter.SerilogAdapter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("658d4b3f-b80f-449f-b57a-d7d91beae73d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
